4dd30089ae65ae1d0d89e4006c30ee9158e7652e
João Pedro Silva <joao.devweb@gmail.com>
2022-01-29 16:02:51 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+  // reactStrictMode: true
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+});
