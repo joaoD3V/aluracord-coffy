f901e47526e038aa56464af0a5b400ab9efc9070
Joao Silva <joao.devweb@gmail.com>
2022-02-12 20:09:51 -0300

    Fix storybook

--- src/components/Message/stories.tsx
+++ src/components/Message/stories.tsx
@@ -6,10 +6,12 @@ export default {
   title: 'Message',
   component: Message,
   args: {
+    id: 1,
     avatar_url: 'https://avatars.githubusercontent.com/u/26118546?v=4',
     login: 'joaoD3V',
-    timestamp: new Date().toJSON(),
+    created_at: new Date().toJSON(),
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate ante ut mi euismod, ut consectetur tortor tincidunt. Aliquam lobortis augue vel diam hendrerit, blandit dictum elit placerat. ',
+    name: 'João Pedro Silva',
   },
 } as Meta;
 
--- src/components/MessageList/stories.tsx
+++ src/components/MessageList/stories.tsx
@@ -2,7 +2,24 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 
 import MessageList, { MessageListProps } from './index';
 
-import { messages } from './messages';
+const messages = [
+  {
+    id: 1,
+    avatar_url: 'https://avatars.githubusercontent.com/u/26118546?v=4',
+    login: 'joaoD3V',
+    created_at: new Date().toJSON(),
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate ante ut mi euismod, ut consectetur tortor tincidunt. Aliquam lobortis augue vel diam hendrerit, blandit dictum elit placerat. ',
+    name: 'João Pedro Silva',
+  },
+  {
+    id: 2,
+    avatar_url: 'https://avatars.githubusercontent.com/u/26118546?v=4',
+    login: 'joaoD3V',
+    created_at: new Date().toJSON(),
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate ante ut mi euismod, ut consectetur tortor tincidunt. Aliquam lobortis augue vel diam hendrerit, blandit dictum elit placerat. ',
+    name: 'João Pedro Silva',
+  },
+];
 
 export default {
   title: 'MessageList',
